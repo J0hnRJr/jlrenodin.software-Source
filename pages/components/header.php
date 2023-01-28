eafaf8f2d031fdd34acde0999fd6669997ec40bf
John R. Jr <53923671+J0hnRJr@users.noreply.github.com>
2023-01-28 15:49:44 -0500

    add navigation in header and a couple incomplete pages (#11)

---
+++ pages/components/header.php
@@ -0,0 +1,10 @@
+<?php
+if (!isset($path_to_root)) {
+    $path_to_root = './';
+}
+?>
+<header>
+    <span>Welcome to John Renodin's Website</span>
+    <br />
+    <?php require_once $path_to_root . 'pages/components/navigation.php'; ?>
+</header>
---
+++ pages/components/incomplete-message.php
@@ -0,0 +1,11 @@
+<h1>Hello</h1>
+<p>This site is still in the design phase. Please check back later.</p>
+<p>Get a
+    <a href="https://github.com/J0hnRJr/jlrenodin.software-Source">
+        sneek peek
+    </a>
+    of potential changes or view the current design.
+</p>
+<p>
+    <a href="https://github.com/J0hnRJr">You can also see what else I am working on!</a>
+</p>

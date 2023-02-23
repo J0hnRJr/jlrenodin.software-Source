334383e128cbbffcf07cee18a5538cbb872f818a
J0hnRjr <jlrenodin@liberty.edu>
2023-02-23 14:59:22 -0500

    add package.json and index.html changes

--- pages/E-Portfolio.php
+++ pages/E-Portfolio.php
@@ -7,7 +7,9 @@
 <body>
     <?php $path_to_root = './../'; ?>
     <?php require_once './components/header.php'; ?>
-    <?php require_once './components/incomplete-message.php'; ?>
+    <ul>
+        <li><a href="<?php echo $path_to_root . 'react src/app.js' ?>">React</a></li>
+    </ul>
     <?php require_once './components/footer.php'; ?>
 </body>
 

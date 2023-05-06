1c9814dd825bf0783fa64ad01cb3d25320494324
J0hnRjr <jlrenodin@liberty.edu>
2023-05-06 15:04:03 -0400

    Add content for experience section

---
+++ pages/components/about/experience.php
@@ -0,0 +1,23 @@
+<?php ?>
+<h2>Software Engineering</h2>
+<p>
+    In the journey of my studies, I have been introduced to many of the modern languages used to build the software being 
+    used in today's architecture. I have worked with both front-end and back-end technologies. I have even worked from 
+    small one hour project to full greater than 160-hour projects both on my own and working with a team. These include a 
+    capstone project for Liberty University and the work I performed for Nuday Tuday to help create the Treasure Hunt 
+    Challenge App.
+</p>
+<h2>Leadership</h2>
+<p>
+    After many years of working in various warehouse settings, I was informed of an opening as a Warehouse Lead at Accredo 
+    Health Group Inc. I decided to apply and was given the opportunity to help lead a team, directing and instructing a 
+    group of over 10 individuals. This was a great chance to learn about how leaders think and what they expect from those 
+    that are following their direction. After leading for two years I decided it was time to focus on my studies and step 
+    down from leadership.
+</p>
+<h2>Warehousing and Logistics</h2>
+<p>
+    I have over 9 years of experience in various logistical and warehouse environments with 8 of those being in warehouses.
+    Stocking, receiving, shipping, returning to stock, and staging are some of the tasks I became extremely familiar with 
+    while working in these locations.
+</p>

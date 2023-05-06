611ce1f1ed89ce64135e4f530504a8149ae20517
J0hnRjr <jlrenodin@liberty.edu>
2023-05-06 14:47:38 -0400

    Add structure of page

--- pages/about.php
+++ pages/about.php
@@ -7,7 +7,12 @@
 <body>
     <?php $path_to_root = './../'; ?>
     <?php require_once './components/header.php'; ?>
-    <?php require_once './components/incomplete-message.php'; ?>
+    <h1>History</h1>
+    <?php include_once './components/about/history.php';?>
+    <h1>Education</h1>
+    <?php include_once './components/about/education.php';?>
+    <h1>Experience</h1>
+    <?php include_once './components/about/experience.php';?>
     <?php require_once './components/footer.php'; ?>
 </body>
 

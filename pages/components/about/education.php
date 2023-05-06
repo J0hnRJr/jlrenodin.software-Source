ed4c6b2a2cf9dfcb54dadccd379e2f33aa7aa640
John R. Jr <53923671+J0hnRJr@users.noreply.github.com>
2023-05-06 15:21:22 -0400

    Add About Content (#25)
    Starting to address #2 by adding more info about me.

---
+++ pages/components/about/education.php
@@ -0,0 +1,5 @@
+<p>
+    I have completed a degree in Bachelor of Science Information Technology: Application and Database Development at Liberty 
+    University. Much of my studies covered the concepts of database design, user experience, code documentation, and software 
+    design.
+</p>

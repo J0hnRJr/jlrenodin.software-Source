3f421a14b4e1e99b610b34b6c6362c3a4380932b
J0hnRjr <jlrenodin@liberty.edu>
2023-05-06 14:48:53 -0400

    Add content for history section

---
+++ pages/components/about/history.php
@@ -0,0 +1,21 @@
+<?php ?>
+<p>
+    I was first introduced to computers by my father, a software engineer at the time. This was primarily entertainment 
+    related, but I quickly became fascinated by just about everything about the software that was on the computer. I 
+    explored countless games and was even shown how to code a webpage in HyperText Markup Language (HTML) and change 
+    the color and layout with Cascading Style Sheets (CSS) during high school.
+</p>
+<p>
+    After graduating High School, I took some time to explore different jobs to see what I would be interested in 
+    pursuing as a career. I tried retail and various warehouse positions and finally settled into a Warehouse 
+    Associate position at Accredo Health Group Inc. After some time, there I simply became bored and wanted to try 
+    something new. I remembered how much enjoyment I had with designing the website in high school and began my journey 
+    to achieve my bachelor's degree. Along the way I met a friend that invited me to help build a mobile app for the 
+    Treasure Hunt Project. After 1.5 years of working to finish the design, we finally made the app available to the 
+    public in August of 2022.
+</p>
+<p>
+    I have also learned much through my studies at Pennsylvania State University and Liberty University. I have taken 
+    introductory classes in many programming languages and a few advanced supplemental courses. I have also studied 
+    design concepts and even built a few of my own designs.
+</p>

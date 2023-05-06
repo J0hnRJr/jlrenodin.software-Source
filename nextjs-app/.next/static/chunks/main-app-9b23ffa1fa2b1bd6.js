392606244780c766c31f1667348ba7a2b1893aa1
J0hnRjr <jlrenodin@liberty.edu>
2023-05-06 13:08:09 -0400

    add .next folder and fix link

---
+++ nextjs-app/.next/static/chunks/main-app-9b23ffa1fa2b1bd6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[744],{3918:function(e,n,t){Promise.resolve().then(t.t.bind(t,3395,23)),Promise.resolve().then(t.t.bind(t,7449,23)),Promise.resolve().then(t.t.bind(t,485,23)),Promise.resolve().then(t.t.bind(t,8668,23))}},function(e){var n=function(n){return e(e.s=n)};e.O(0,[679],function(){return n(1070),n(3918)}),_N_E=e.O()}]);

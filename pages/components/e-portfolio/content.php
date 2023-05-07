fb43113a43374a4afcdbb0ed5b05c9c965dc8a12
J0hnRjr <jlrenodin@liberty.edu>
2023-05-07 14:09:47 -0400

    Add incomplete pages and links

--- pages/components/e-portfolio/content.php
+++ pages/components/e-portfolio/content.php
@@ -4,6 +4,7 @@
     <li><a href="https://www.codecademy.com/profiles/J0hnRJr">Codecademy</a></li>    
     <li><a href="https://jlrenodin.software/nextjs-app/.next/server/pages/homepage.html">React for browser Apps</a></li>
     <li><a href="https://www.treasurehuntproject.com/">React for Mobile Apps</a></li>
-    <!-- <li><a href="https://jlrenodin.software/pages/transcripts.php">Download University Transcripts</a></li> -->
+    <li><a href="https://jlrenodin.software/pages/Capstone-Project.php">C# (.NET)</a></li>
+    <li><a href="https://jlrenodin.software/pages/transcripts.php">Download University Transcripts</a></li>
     <li><a href="https://wakatime.com/@J0hnRJr">WakaTime</a></li>
 </ul>

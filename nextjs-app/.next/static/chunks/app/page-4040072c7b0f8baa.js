947e2eb0367366df90bbb51048b6658f17a33d47
John R. Jr <53923671+J0hnRJr@users.noreply.github.com>
2023-05-24 11:15:07 -0400

    Add more react content and two links to other pages (#40)
    Starts to address #5

--- nextjs-app/.next/static/chunks/app/page-4040072c7b0f8baa.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{8170:function(n,e,u){Promise.resolve().then(u.t.bind(u,2104,23))}},function(n){n.O(0,[104,679,744],function(){return n(n.s=8170)}),_N_E=n.O()}]);
--- nextjs-app/src/pages/homepage.tsx
+++ nextjs-app/src/pages/homepage.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+"use client";
+import React, {useState} from "react";
 import Link from "next/link";
 
 const ExampleApp = () => {
   const headerText = "React";
-  const contentText =
-    "This page is generated using React and Typescript. See the source code at ";
-  const sourceText =
-    "https://github.com/J0hnRJr/jlrenodin.software-Source/blob/main/nextjs-app/src/app/page.tsx";
+  const [contentText, setContentText] = useState(
+    "This page is generated using React and Typescript. See the source code at "
+  );
+  const sourceText = "https://github.com/J0hnRJr/jlrenodin.software-Source/blob/main/nextjs-app/src/app/page.tsx";
+  const [count, setCount] = useState(0);
+
   return (
     <div style={{padding:'1%'}}>
       <h1>{headerText}</h1>
@@ -18,6 +21,11 @@ const ExampleApp = () => {
         >{"this page"}
         </Link>
       </p>
+      <button onClick={()=> {
+        setCount((prev)=>{return prev+1});
+        setContentText("You have changed the page content " + count + " times!");
+      }}>Click here to change the page</button>
+      <p><a href="https://jlrenodin.software/">Home</a> | <a href="https://www.jlrenodin.software/pages/E-Portfolio.php">Go back</a></p>
     </div>
   );
 };

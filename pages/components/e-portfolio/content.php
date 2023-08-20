4dd49426e004c6ca17791f3dd12594dfeac654e9
John Renodin Jr <jlrenodin@liberty.edu>
2023-08-19 23:42:49 -0400

    Add experience at Westelcom

--- pages/components/e-portfolio/content.php
+++ pages/components/e-portfolio/content.php
@@ -1,5 +1,6 @@
 <?php ?>
 <ul>
+    <li>Software Engineer I at <a href="https://westelcom.com/our-company/">Westelcom</a></li>
     <li><a href="https://github.com/J0hnRJr/">GitHub</a></li>
     <li><a href="https://www.codecademy.com/profiles/J0hnRJr">Codecademy</a></li>    
     <li><a href="https://jlrenodin.software/nextjs-app/.next/server/pages/homepage.html">React for browser Apps</a></li>

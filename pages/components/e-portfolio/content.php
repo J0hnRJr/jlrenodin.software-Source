186a3bb2f4f9ff1ea8055dafc77443e1f434b707
John Renodin Jr <jlrenodin@liberty.edu>
2023-08-19 23:44:17 -0400

    Reorganize list of links.

--- pages/components/e-portfolio/content.php
+++ pages/components/e-portfolio/content.php
@@ -2,9 +2,9 @@
 <ul>
     <li>Software Engineer I at <a href="https://westelcom.com/our-company/">Westelcom</a></li>
     <li><a href="https://github.com/J0hnRJr/">GitHub</a></li>
-    <li><a href="https://www.codecademy.com/profiles/J0hnRJr">Codecademy</a></li>    
-    <li><a href="https://jlrenodin.software/nextjs-app/.next/server/pages/homepage.html">React for browser Apps</a></li>
     <li><a href="https://www.treasurehuntproject.com/">React for Mobile Apps</a></li>
+    <li><a href="https://jlrenodin.software/nextjs-app/.next/server/pages/homepage.html">React for browser Apps</a></li>
+    <li><a href="https://www.codecademy.com/profiles/J0hnRJr">Codecademy</a></li>
     <li><a href="https://jlrenodin.software/pages/Capstone-Project.php">C# (.NET)</a></li>
     <li><a href="https://jlrenodin.software/pages/transcripts.php">Download University Transcripts</a></li>
     <li><a href="https://wakatime.com/@J0hnRJr">WakaTime</a></li>

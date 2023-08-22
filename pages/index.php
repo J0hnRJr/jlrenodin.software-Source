1891cad88c817437ca32690c7ec5e6d1534820cb
John Renodin Jr <jlrenodin@liberty.edu>
2023-08-21 21:38:25 -0400

    Fix relative links to index.php

--- pages/index.php
+++ pages/index.php
@@ -3,13 +3,14 @@
 <head>
   <title>John's Homepage</title>
   <meta google-site-verification="2rV_aGPsh2AP8EDqnC4xoDLoYWckIo3J-6tzWuN_WiQ" />
-  <link rel="stylesheet" type="text/css" href="./pages/styles/default.css" />
+  <link rel="stylesheet" type="text/css" href="./styles/default.css" />
 </head>
 
 <body>
-  <?php require_once './pages/components/header.php'; ?>
-  <?php require_once './pages/components/incomplete-message.php'; ?>
-  <?php require_once './pages/components/footer.php'; ?>
+  <?php $path_to_root = './../'; ?>
+  <?php require_once './components/header.php'; ?>
+  <?php require_once './components/incomplete-message.php'; ?>
+  <?php require_once './components/footer.php'; ?>
 </body>
 
 </html>

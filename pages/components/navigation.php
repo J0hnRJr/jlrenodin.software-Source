d578cd0f61bb5f018c3c3cd0bb216849a7351c6e
John R. Jr <53923671+J0hnRJr@users.noreply.github.com>
2023-08-21 21:39:37 -0400

    Fix relative links to index.php (#44)

--- pages/components/navigation.php
+++ pages/components/navigation.php
@@ -2,7 +2,7 @@
 require 'root-path.php';
 ?>
 <span>
-    <a href="<?php echo $path_to_root ?>index.php">Home</a> |
+    <a href="<?php echo $path_to_root ?>pages/index.php">Home</a> |
     <a href="<?php echo $path_to_root ?>pages/E-Portfolio.php">E-Portfolio</a> |
     <a href="<?php echo $path_to_root ?>pages/about.php">About</a>
 </span>

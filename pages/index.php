b402c5d9b16a57209b214353ba18cfb21d0a7776
John R. Jr <53923671+J0hnRJr@users.noreply.github.com>
2023-08-21 21:21:11 -0400

    Fix index deployment errors (#43)
    Remove extra hidden character in index.php and reorganize with a
    redirect to new index location.

---
+++ pages/index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <title>John's Homepage</title>
+  <meta google-site-verification="2rV_aGPsh2AP8EDqnC4xoDLoYWckIo3J-6tzWuN_WiQ" />
+  <link rel="stylesheet" type="text/css" href="./pages/styles/default.css" />
+</head>
+
+<body>
+  <?php require_once './pages/components/header.php'; ?>
+  <?php require_once './pages/components/incomplete-message.php'; ?>
+  <?php require_once './pages/components/footer.php'; ?>
+</body>
+
+</html>

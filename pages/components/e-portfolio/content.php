2e439aea568f9b7de5c9b8936cc8e53911ada636
John Renodin Jr <jlrenodin@liberty.edu>
2024-06-11 22:51:29 -0400

    Add PMP certification

--- pages/components/e-portfolio/content.php
+++ pages/components/e-portfolio/content.php
@@ -3,6 +3,8 @@
 <p>These are some links to my work:</p>
 <ul>
     <li>Software Engineer I at <a href="https://westelcom.com/our-company/">Westelcom</a></li>
+    <li>Certified Project Management Professional</li>
+    <div data-iframe-width="300" data-iframe-height="200" data-share-badge-id="82bd48d3-db0f-4592-b3d8-c988e6e594e1" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
     <li><a href="https://github.com/J0hnRJr/">GitHub</a></li>
     <li><a href="https://www.treasurehuntproject.com/">React for Mobile Apps</a></li>
     <li><a href="https://jlrenodin.software/nextjs-app/.next/server/pages/homepage.html">React for browser Apps</a></li>
